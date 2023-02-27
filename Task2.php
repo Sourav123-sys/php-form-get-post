b58d4321a24cc9bda07cb6a34f8b25a609ef921f
Sourav123-sys <khansourav58@gmail.com>
2023-02-27 21:59:14 +0600

    complete

--- Task2.php
+++ Task2.php
@@ -1,6 +1,24 @@
 <?php
 
+// Create a PHP class called "Person" with the following properties and methods:
 
+// Properties:
+
+// Name
+
+// Email
+
+// Methods:
+
+// setName($name): sets the name property
+
+// setEmail($email): sets the email property
+
+// getName(): returns the name property
+
+// getEmail(): returns the email property
+
+// Create an instance of the Person class and set the name and email properties. Use the getName() and getEmail() methods to display the properties on the webpage.
 
 class Person {
     private $name;
